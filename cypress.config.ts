68af504f4a870c77a2f2783d33878f0193a1ebc1
MariaJose319 <mjsierra319@gmail.com>
2022-07-21 17:12:22 -0500

    Continuous Integration (#6)

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,8 @@ export default defineConfig({
   // setupNodeEvents can be defined in either
   // the e2e or component configuration
   e2e: {
+    video: false,
+    screenshotOnRunFailure: false,
     setupNodeEvents(on, config) {
       // modify config values examples
       // config.defaultCommandTimeout = 10000
